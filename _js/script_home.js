8599bb8fbf6d6425ccb777090e534a01a2f1ff3c
thiago <thiagotcs@outlook.com>
2019-11-11 09:03:10 -0300

    primeiro commit

---
+++ _js/script_home.js
@@ -0,0 +1,5 @@
+const cards = document.querySelectorAll('.memory-card');
+function flipCard() {
+  this.classList.toggle('flip');
+}
+cards.forEach(card => card.addEventListener('click', flipCard));
